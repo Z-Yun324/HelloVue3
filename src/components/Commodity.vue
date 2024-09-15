b3a405c7f9d791ae5284790e3c69556f45e715f8
ZY324 <emma93324@gamil.com>
2024-09-15 20:51:03 +0800

    Feat: 基本類型響應式

--- src/components/Commodity.vue
+++ src/components/Commodity.vue
@@ -3,29 +3,33 @@
   <div class="commodity">
     <h3>{{ brand }}</h3>
     <h2>{{ name }}</h2>
-    <h2>{{ price }}</h2>
+    <h2>數量 {{ quantity }}</h2>
+    <h2>價錢 $ {{ price }}</h2>
+    <h2>總價錢 $ {{ comTotal }}</h2>
+    <button @click="addCom">增加</button>
     <button @click="deleteCom">刪除</button>
   </div>
 </template>
 
-<!--JS/TS-->
-<script lang="ts">
-export default {
-  name: "Commodity",
-  data() {
-    return {
-      brand: "APPLE",
-      name: "IPONE16",
-      quantity: 0,
-      price: 50000,
-    };
-  },
-  methods: {
-    deleteCom() {
-      alert("已刪除");
-    },
-  },
-};
+<script lang="ts" setup name="commodity">
+import { computed, ref } from "vue";
+//data no響應式
+let brand = "APPLE";
+let name = "IPONE16";
+let quantity = ref(0);
+let price = 50000;
+const comTotal = computed(() => quantity.value * price);
+//function
+function deleteCom() {
+  if (quantity.value > 0) {
+    quantity.value -= 1;
+  } else {
+    alert("數量已為零");
+  }
+}
+function addCom() {
+  quantity.value += 1;
+}
 </script>
 
 <!--樣式-->
