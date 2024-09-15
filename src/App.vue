0394970ed95c19c6219b82a791d01d29ec206b90
ZY324 <emma93324@gamil.com>
2024-09-15 18:18:53 +0800

    Feat 新增Commodity

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="car">
+    <h1>購物車</h1>
+    <Commodity />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+import Commodity from "./components/Commodity.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: "App",
+  components: { Commodity },
+};
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+.car {
+  background-color: rgb(87, 187, 187);
+  padding: 20px;
 }
 </style>
---
+++ src/components/Commodity.vue
@@ -0,0 +1,37 @@
+<!--HTML-->
+<template>
+  <div class="commodity">
+    <h3>{{ brand }}</h3>
+    <h2>{{ name }}</h2>
+    <h2>{{ price }}</h2>
+    <button @click="deleteCom">刪除</button>
+  </div>
+</template>
+
+<!--JS/TS-->
+<script lang="ts">
+export default {
+  name: "Commodity",
+  data() {
+    return {
+      brand: "APPLE",
+      name: "IPONE16",
+      quantity: 0,
+      price: 50000,
+    };
+  },
+  methods: {
+    deleteCom() {
+      alert("已刪除");
+    },
+  },
+};
+</script>
+
+<!--樣式-->
+<style scoped>
+.commodity {
+  background-color: #fff;
+  padding: 20px;
+}
+</style>
